ad5c6776d7e9322bf3a78163d6cfc4fbbff70570
Decken Kang <deckenkang66@blueplanet.com.tw>
2018-01-15 00:31:44 +0800

    back up stockInstitutionalInvestor

--- backUpSQL.bat
+++ backUpSQL.bat
@@ -2,3 +2,4 @@ cd "C:\Program Files\MariaDB 10.2\bin"
 mysqldump -u root -p89787198 stockevaluation stockdata > C:\\Users\\blueplanet\\PycharmProjects\\stockdata-%date:~0,4%-%date:~5,2%-%date:~8,2%.sql
 mysqldump -u root -p89787198 stockevaluation stockproxies > C:\\Users\\blueplanet\\PycharmProjects\\stockproxies-%date:~0,4%-%date:~5,2%-%date:~8,2%.sql
 mysqldump -u root -p89787198 stockevaluation stocktable > C:\\Users\\blueplanet\\PycharmProjects\\stocktable-%date:~0,4%-%date:~5,2%-%date:~8,2%.sql
+mysqldump -u root -p89787198 stockevaluation stockinstitutionalinvestor > C:\\Users\\blueplanet\\PycharmProjects\\stockinstitutionalinvestor-%date:~0,4%-%date:~5,2%-%date:~8,2%.sql
